2931e0e5bcea1af96415c8ac4a68e8f2dd0ae12e
arthurlilintek <arthurlilintek@users.noreply.github.com>
2018-06-08 17:17:24 +0800

    Update MainActivity.java

--- app_listcoffee/src/main/java/book/lab/com/app_listcoffee/MainActivity.java
+++ app_listcoffee/src/main/java/book/lab/com/app_listcoffee/MainActivity.java
@@ -1,13 +1,42 @@
 package book.lab.com.app_listcoffee;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.CursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private Context context;
+    private ListView listView = null;
+    private Uri coffeeURI = Uri
+            .parse("content://com.lilintek.app_sqlitecp.utils.CoffeeProvider");
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setEmptyView(findViewById(R.id.emptyView));
+// 重新整理ListView
+        refreshListView();
+    }
+    // 重新整理ListView（將資料重新匯入）
+    private void refreshListView() {
+        Cursor cursor = getContentResolver().query(
+                coffeeURI,
+                new String[] { "_id", "title", "price", "image" },
+                null, null, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                context,R.layout.row,
+                cursor,
+                new String[] { "_id", "title", "price", "image" },
+                new int[] { R.id.itemId, R.id.itemTitle,
+                        R.id.itemPrice, R.id.itemImage },
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        listView.setAdapter(adapter);
     }
 }
